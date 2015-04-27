b2ef31f562ef0ca6671699b1933648185fd87779
starboychina <admin@kansea.com>
2015-04-27 16:19:19 +0900

    search

--- app/controllers/mobile/station.js
+++ app/controllers/mobile/station.js
@@ -1,26 +1,28 @@
 // Create new sub document
-var Line = require('mongoose').model('Line');
-
+var mongoose = require('mongoose'),
+	Line = mongoose.model('Line'),
+	Station = mongoose.model('Line');
 module.exports = function(req, res, next) {
 	req.body = req.query; //テスト
-	var oneStation = {};
+
+	var condition_station = {};
 	for (var index in req.body) {
 		if ( /stations./.test(index)){
-			oneStation = {
-				'name':true,
-				'pref':true,
-				'stations.$': true};
-			break;
+			var key = index.replace(/stations./,"");
+			condition_station[key] = new RegExp('^.*'+req.body[index]+'.*$', "i");
 		}
 	};
 
-	var query = Line.find(req.body,oneStation);
-		query.populate('stations')
-		        .exec(function(err, posts) {
+	var query = Line.find();
+		query.populate('stations',{},condition_station)
+		        .exec(function(err, lines) {
 		            if (err) next(err);
-		            else if (posts.length === 0) res.json(404, {});
+		            else if (lines.length === 0) res.json(404, {});
 		            else {
-    					res.json(200, posts);
+		            	 lines = lines.filter(function(line){
+						     return line.stations.length;
+						   })
+    					res.json(200, lines);
 		            }
 		        });
     //res.json(400, "ok");
