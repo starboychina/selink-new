1d43794dbbd6b7519affa08a5d60918182f3dd19
Xinguang Wang <admin@kansea.com>
2015-05-22 17:40:43 +0900

    res.redirect

--- app/controllers/mobile/sharePost.js
+++ app/controllers/mobile/sharePost.js
@@ -9,17 +9,17 @@ var populateField = {
 };
 
 module.exports = function(req, res, next) {
-    if(req.query.isappinstalled==1){
-        res.redirect('genbatomo://post/'+req.params.id)
-    }else{
-        Post.findById(req.params.id)
-            .where('logicDelete').equals(false)
-            .populate('_owner', 'type firstName lastName title cover photo createDate')
-            .populate('group', 'name cover description')
-            .populate('comments._owner', 'type firstName lastName title cover photo createDate')
-            .exec(function(err, post) {
-                if (err) next(err);
-                else res.render('./share/post', post);
-            });
-    }
+    // if(req.query.isappinstalled==1){
+    //     res.redirect('genbatomo://post/'+req.params.id)
+    //     return;
+    // }
+    Post.findById(req.params.id)
+        .where('logicDelete').equals(false)
+        .populate('_owner', 'type firstName lastName title cover photo createDate')
+        .populate('group', 'name cover description')
+        .populate('comments._owner', 'type firstName lastName title cover photo createDate')
+        .exec(function(err, post) {
+            if (err) next(err);
+            else res.render('./share/post', post);
+        });
 };
