9d5b04fa56e98474dd2ca393b4fc54c2e54b4083
Xinguang Wang <admin@kansea.com>
2015-05-27 16:33:23 +0900

    user stations

--- app/controllers/user/show.js
+++ app/controllers/user/show.js
@@ -7,8 +7,10 @@ module.exports = function(req, res, next) {
 
     // check on logic delete flag, return 404 on not found
 
-    User.findById(req.params.user, '-password -logicDelete -openids', function(err, user) {
-        if (err) next(err);
-        else res.json(user);
-    });
+    User.findById(req.params.user, '-password -logicDelete -openids')
+    	.populate('stations')
+        .exec(function(err, user) {
+	        if (err) next(err);
+	        else res.json(user);
+	    });
 };
