362b7f4747f5e57264a8f923f37c246824349e94
Xinguang Wang <admin@kansea.com>
2015-08-07 13:12:33 +0900

    logout -> remove device

--- app/controllers/user/logout.js
+++ app/controllers/user/logout.js
@@ -1,8 +1,24 @@
 // User logout
-var Activity = require('mongoose').model('Activity');
+var mongoose = require('mongoose'),
+    Activity = mongoose.model('Activity'),
+    User = mongoose.model('User');
 
 module.exports = function(req, res, next) {
 
+    if (req.query.token){
+
+        User.findById(req.session.userId, function(err, user){
+
+            user.devices.forEach(function(device) {
+                if(device.token == req.query.token){
+                    
+                    device.remove();
+
+                    user.save();
+                }
+            });
+        });
+    }
     // create activity
     Activity.create({
         _owner: req.session.userId,
@@ -10,18 +26,6 @@ module.exports = function(req, res, next) {
     }, function(err, activity) {
         if (err) next(err);
     });
-
-	if (req.body.token){
-        console.log(req.user.devices)
-		req.user.devices.forEach(function(device) {
-
-            console.log(device)
-            console.log(token)
-            if(device.token == req.body.token){
-            	req.user.devices.pull(device);
-            }
-        });
-	}
 	
     req.session.destroy();
     res.redirect('/');
