d2ea537ea868f9c48f71cedae34cef1cd7f4be6f
Joe Hetfield <joe_19840729@hotmail.com>
2015-08-13 11:14:13 +0900

    fix

--- app/controllers/message/chat.js
+++ app/controllers/message/chat.js
@@ -1,24 +1,40 @@
-var moment = require('moment'),
+var async = require('async'),
+    moment = require('moment'),
     Message = require('mongoose').model('Message');
 
 module.exports = function(req, res, next) {
 
-    // create query
-    var query = Message.find();
+    async.parallel({
 
-    // if request items before some time point
-    if (req.query.before)
-        query.where('createDate').lt(moment.unix(req.query.before).toDate());
+        openMessages: function(callback) {
+            Message.where('_from').equals(req.params.user)
+                .where('_recipient').equals(req.user.id)
+                .where('logicDelete').ne(req.user.id)
+                .setOptions({ multi: true })
+                .update({ $addToSet: {opened: req.user.id} }, callback);
+        },
 
-    query.select('_recipient _from content opened createDate')
-        .where('_from').in([req.user.id, req.params.user])
-        .where('_recipient').in([req.user.id, req.params.user])
-        .where('logicDelete').ne(req.user.id)
-        .limit(req.query.size || 20)
-        .sort('-createDate')
-        .exec(function(err, messages) {
-            if (err) next(err);
-            else if (messages.length === 0) res.json(404, {});
-            else res.json(messages);
-        });
+        readMessages: function(callback) {
+
+            // create query
+            var query = Message.find();
+
+            // if request items before some time point
+            if (req.query.before)
+                query.where('createDate').lt(moment.unix(req.query.before).toDate());
+
+            query.select('_recipient _from content opened createDate')
+                .where('_from').in([req.user.id, req.params.user])
+                .where('_recipient').in([req.user.id, req.params.user])
+                .where('logicDelete').ne(req.user.id)
+                .limit(req.query.size || 20)
+                .sort('-createDate')
+                .exec(callback);
+        }
+
+    }, function(err, result) {
+        if (err) next(err);
+        else if (result.readMessages.length === 0) res.json(404, {});
+        else res.json(result.readMessages);
+    });
 };
