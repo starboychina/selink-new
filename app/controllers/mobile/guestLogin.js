af37aadc18deb1ee13a45c4d8ba19f97870677f7
Xinguang Wang <admin@kansea.com>
2015-08-21 20:46:08 +0900

    fix

--- app/controllers/mobile/guestLogin.js
+++ app/controllers/mobile/guestLogin.js
@@ -11,7 +11,7 @@ module.exports = function(req, res, next) {
   User.findById(req.body.tomoid, function(err, user) {
 
       if (err) next(err);
-      else if (user == null) autoRegist
+      else if (user == null) autoRegist(req, res, next)
       else {
           expandUserObject(user);
       }
